a4190e7951ddf64ff00109c668def3f2f3809db1
Andy Smith <andy@wolfsmart.net>
2017-06-03 18:40:17 +0100

    waiting for the promise of .netstandard 2.0 perhaps.  define PORTABLE..

---
+++ Test.YAML.Net/TemplateTest.cs
@@ -0,0 +1,14 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test.YAML.Net
+{
+    [TestClass]
+    public class TemplateTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
